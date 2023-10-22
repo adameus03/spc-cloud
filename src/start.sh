f3e1921ff5fcdbe6b70288cafed82f82f824f73d
mundus <mundus@pc6.home>
2023-10-22 23:09:09 +0200

    Enabled apk update && apk upgrade to fail in start.sh to become compatible with systems not supporting apk

--- src/start.sh
+++ src/start.sh
@@ -6,30 +6,27 @@ apk update && apk upgrade
 #echo "Start script: starting watch whoami in background"
 #watch whoami
 
-if [ $? -eq 0 ]; then
-    echo "$SCRIPT_NICKNAME: Setting environment variables..."
-    chmod u+rx ./config.sh
-    source ./config.sh
-    if [ $? -eq 0 ]; then
-        /bin/sh $VOLUME_GUARD_PATH
-        if [ $? -eq 0 ]; then
-            echo "$SCRIPT_NICKNAME: setup db sync..."
-            npm run migrate
-
-            if [ $? -eq 0 ]; then
-                echo "$SCRIPT_NICKNAME: Launching core application..."
-                npm run pm2
-            else
-                echo "$SCRIPT_NICKNAME: ERROR - Failed to setup db sync"
-            fi
-        else
-            echo "$SCRIPT_NICKNAME: ERROR - Persistent volume guard error" 
-        fi
-    else 
-        echo "$SCRIPT_NICKNAME: ERROR - Failed to set environment variables, check for corrupted or inaccessible config.sh file"
-    fi
-else
-    echo "$SCRIPT_NICKNAME: ERROR - Failed to update/upgrade the packages"
+if [ ! $? -eq 0 ]; then
+    echo "$SCRIPT_NICKNAME: ERROR - Failed to update/upgrade the packages. Proceeding with the current version."
 fi
+echo "$SCRIPT_NICKNAME: Setting environment variables..."
+chmod u+rx ./config.sh
+source ./config.sh
+if [ $? -eq 0 ]; then
+    /bin/sh $VOLUME_GUARD_PATH
+    if [ $? -eq 0 ]; then
+        echo "$SCRIPT_NICKNAME: setup db sync..."
+        npm run migrate
 
-
+        if [ $? -eq 0 ]; then
+            echo "$SCRIPT_NICKNAME: Launching core application..."
+            npm run pm2
+        else
+            echo "$SCRIPT_NICKNAME: ERROR - Failed to setup db sync"
+        fi
+    else
+        echo "$SCRIPT_NICKNAME: ERROR - Persistent volume guard error" 
+    fi
+else 
+    echo "$SCRIPT_NICKNAME: ERROR - Failed to set environment variables, check for corrupted or inaccessible config.sh file"
+fi
