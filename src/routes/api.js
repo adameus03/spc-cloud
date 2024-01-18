15421e90392a2e6f9c2202243fe99f45a236ba07
mundus <mundus@pc6.home>
2024-01-18 01:35:57 +0100

    Can upload multiple files at once

--- src/routes/api.js
+++ src/routes/api.js
@@ -10,28 +10,41 @@ router.post('/upload', (req, res, next) => {
     const form = new formidable.IncomingForm();
     form.parse(req, async function(err, fields, files){
       if(files.filetoupload){
-        let oldPath = files.filetoupload[0].filepath;
-
         let session = await db.LoginInstance.findByPk(req.cookies["login_id"]);
+        
+        let uploadSucceeded = true;
+        for (let file of files.filetoupload) {
+          /*let oldpath = file.path;
+          let newpath = `${process.env.USRFILES_LOCATION}/${session.user_id}/${file.name}`;
+          fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+          });*/
+          let oldPath = file.filepath;
+          let newPath = `${process.env.USRFILES_LOCATION}/${session.user_id}/${file.originalFilename}`;
+          console.log(`FILE UPLOAD ${newPath}`);
+          let rawData = fs.readFileSync(oldPath);
+          
+          fs.writeFile(newPath, rawData, function(err){
+              if(err) {
+                console.log(`Error while uploading to ${newPath}: ${err}`);
+                uploadSucceeded = false;
+              }
+              else {
+                console.log(`Successfully uploaded to ${newPath}`); 
+                //return res.send("Successfully uploaded");
+              }
+              
+          });
+        }
 
-        let newPath = `${process.env.USRFILES_LOCATION}/${session.user_id}/${files.filetoupload[0].originalFilename}`;
-        console.log(`FILE UPLOAD ${newPath}`);
-        let rawData = fs.readFileSync(oldPath);
-      
-        fs.writeFile(newPath, rawData, function(err){
-            if(err) {
-              console.log(err);
-              res.locals.error = "Upload with an error.";
-              res.render('transfer-gui.html', { title: 'Transfer' });
-              //return res.send("Upload with an error")
-            }
-            else {
-              res.locals.success = "Successfully uploaded the file.";
-              res.render('transfer-gui.html', { title: 'Transfer' });
-              //return res.send("Successfully uploaded");
-            }
-            
-        });
+        if (uploadSucceeded) {
+          res.locals.success = files.filetoupload.length === 1 ? "Successfully uploaded the file." : `Successfully uploaded ${files.filetoupload.length} files.`;
+          res.render('transfer-gui.html', { title: 'Transfer' });
+        } else {
+          res.locals.error = "One or more files failed to upload";
+          res.render('transfer-gui.html', { title: 'Transfer' });
+        }
+
       }
       else {
         return res.send("No file / empty file attached")
