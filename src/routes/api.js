ed7b021ba06cc0ad745ac1aab28d26bf2f8f1242
mundus <mundus@pc6.home>
2024-01-18 22:19:17 +0100

    Client changes autocommit

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,6 +6,7 @@ var getDirName = require('path').dirname;
 const formidable = require('formidable');
 const db = require('../database.js');
 const archiver = require('archiver');
+const shell = require('../shell.js');
 
 var router = express.Router();
 
@@ -47,6 +48,19 @@ function zipDirectory(sourceDir, outPath) {
 
 const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
 
+function writeFileAsync(path, rawData, err_cb) {
+  return new Promise((resolve, reject) => {
+    writeFile(path, rawData, (err) => {
+      if (err) {
+        err_cb(err);
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
 router.post('/upload', (req, res, next) => {
     const form = new formidable.IncomingForm();
     form.parse(req, async function(err, fields, files){
@@ -54,18 +68,37 @@ router.post('/upload', (req, res, next) => {
         let session = await db.LoginInstance.findByPk(req.cookies["login_id"]);
         
         let uploadSucceeded = true;
-        for (let file of files.filetoupload) {
+        let theseAreFileReplaces = [];
+        //let oldPaths = [];
+
+        for (let i = 0; i < files.filetoupload.length; i++) {
+          let file = files.filetoupload[i];
           /*let oldpath = file.path;
           let newpath = `${process.env.USRFILES_LOCATION}/${session.user_id}/${file.name}`;
           fs.rename(oldpath, newpath, function (err) {
             if (err) throw err;
           });*/
           let oldPath = file.filepath;
+          //oldPaths.push(oldPath);
           let newPath = `${process.env.USRFILES_LOCATION}/${session.user_id}/${file.originalFilename}`;
+
           console.log(`FILE UPLOAD ${newPath}`);
           let rawData = fs.readFileSync(oldPath);
+
+          let thisIsFileReplace = fs.existsSync(newPath);
+          theseAreFileReplaces.push(thisIsFileReplace);
           
-          writeFile(newPath, rawData, function(err){
+          await writeFileAsync(newPath, rawData, (err) => {
+            if(err) {
+              console.log(`Error while uploading to ${newPath}: ${err}`);
+              uploadSucceeded = false;
+            }
+            else {
+              console.log(`Successfully uploaded to ${newPath}`); 
+              //return res.send("Successfully uploaded");
+            }
+          });
+          /*writeFile(newPath, rawData, function(err){
               if(err) {
                 console.log(`Error while uploading to ${newPath}: ${err}`);
                 uploadSucceeded = false;
@@ -75,15 +108,31 @@ router.post('/upload', (req, res, next) => {
                 //return res.send("Successfully uploaded");
               }
               
-          });
+          });*/
         }
 
         if (uploadSucceeded) {
           res.locals.success = files.filetoupload.length === 1 ? "Successfully uploaded the file." : `Successfully uploaded ${files.filetoupload.length} files.`;
-          res.render('transfer-gui.html', { title: 'Transfer' });
-        } else {
+          //shell.gitCommitFileUpload(newPath, file.originalFilename, thisIsFileReplace).then(() => {
+          shell.gitCommitFileUpload(`${process.env.USRFILES_LOCATION}/${session.user_id}`, files.filetoupload.map((file) => { return file.originalFilename; }), theseAreFileReplaces).then(() => {
+          //shell.gitCommitFileUpload(`${process.env.USRFILES_LOCATION}/${session.user_id}`, oldPaths, theseAreFileReplaces).then(() => {
+            res.render('transfer-gui.html', { title: 'Transfer' });
+          }).catch((err) => {
+            console.log(`git commit error: ${err}`);
+            res.locals.success += " However, an error occured while committing the changes!";
+            res.render('transfer-gui.html', { title: 'Transfer' });
+          });
+          
+        } else { // Let's hope this never happens, but just in case
           res.locals.error = "One or more files failed to upload";
-          res.render('transfer-gui.html', { title: 'Transfer' });
+          shell.gitCommitIncompleteFileUpload(`${process.env.USRFILES_LOCATION}/${session.user_id}`).then(() => {
+            res.render('transfer-gui.html', { title: 'Transfer' });
+          }).catch((err) => {
+            console.log(`git commit error: ${err}`);
+            res.locals.error += " And an error occured while reverting the changes!";
+            res.render('transfer-gui.html', { title: 'Transfer' });
+          });
+          
         }
 
       }
@@ -98,21 +147,39 @@ router.post('/delete', async (req, res) => {
   form.parse(req, async function(err, fields, files) {
     if(fields.f){
       let session = await db.LoginInstance.findByPk(req.cookies["login_id"]);
-      let filepath = `${process.env.USRFILES_LOCATION}/${session.user_id}/${fields.f}`;
+      let filepath;
+
+      //convert fields.f to string
+      let fields_f = fields.f.toString();
+      if (fields_f.startsWith('/')) {
+        filepath = `${process.env.USRFILES_LOCATION}/${session.user_id}${fields.f}`;
+      } else {
+        filepath = `${process.env.USRFILES_LOCATION}/${session.user_id}/${fields.f}`;
+      }
       if(fs.existsSync(filepath)){
         console.log(`file exists: ${filepath}`);
         if (!fs.lstatSync(filepath).isDirectory()) {
           console.log(`FILE DELETE ${filepath}`);
           fs.unlinkSync(filepath);
+          shell.gitCommitFileDelete(`${process.env.USRFILES_LOCATION}/${session.user_id}`, filepath, fields.f).then(() => {
+            res.send({ success: "Successfully deleted the file."});
+          }).catch((err) => {
+            res.send({ success: "Successfully deleted the file, but an error occured while committing the changes!!!"});
+          });
           //res.locals.success = "Successfully deleted the file.";
           //res.render('transfer-gui.html', { title: 'Transfer' });
-          res.send({ success: "Successfully deleted the file."});
+          
         } else {
           console.log(`DIRECTORY DELETE ${filepath}`);
           fs.rmdirSync(filepath, { recursive: true });
+          shell.gitCommitDirDelete(`${process.env.USRFILES_LOCATION}/${session.user_id}`, filepath, fields.f).then(() => {
+            res.send({ success: "Successfully deleted the directory."});
+          }).catch((err) => {
+            res.send({ success: "Successfully deleted the directory, but an error occured while committing the changes!!!"});
+          });
           //res.locals.success = "Successfully deleted the directory.";
           //res.render('transfer-gui.html', { title: 'Transfer' });
-          res.send({ success: "Successfully deleted the directory."});
+          
         }
         
         //res.render('transfer-gui.html', { title: 'Transfer' });
--- src/shell.js
+++ src/shell.js
@@ -18,7 +18,38 @@ function gitInit(pwd) {
     return executeShellCommand(`git init ${pwd}`);
 }
 
+function gitCommitFileUpload(pwd, fnames, areReplaces) {
+    //return executeShellCommand(`git add ${totalPath} && git commit -m "${isReplace ? 'Changed contents of ' : 'Uploaded'} ${fname}"`);
+    let commitMessage = '';
+    for (let i = 0; i < fnames.length - 1; i++) {
+        commitMessage += `${areReplaces[i] ? 'Changed contents of ' : 'Uploaded'} ${fnames[i]}, `;
+    }
+    commitMessage += `${areReplaces[fnames.length - 1] ? 'Changed contents of ' : 'Uploaded'} ${fnames[fnames.length - 1]}`;
+    return executeShellCommand(`cd ${pwd} && git add --all && git commit -m "${commitMessage}"`);
+}
+
+function gitCommitIncompleteFileUpload(pwd) {
+    // this is beautiful
+    return executeShellCommand(`cd ${pwd} && git reset --hard`);   
+}
+
+function gitCommitFileDelete(pwd, totalPath, fname) {
+    //return executeShellCommand(`git rm ${totalPath} && git commit -m "Deleted file: ${fname}"`);
+    //return executeShellCommand(`cd ${pwd} && rm ${totalPath} && git commit -m "Deleted file: ${fname}"`);
+    return executeShellCommand(`cd ${pwd} && git add --all && git commit -m "Deleted file: ${fname}"`);
+}
+
+function gitCommitDirDelete(pwd, totalPath, fname) {
+    //return executeShellCommand(`cd ${pwd} && rm -rf ${totalPath} && git commit -m "Deleted whole directory: ${fname}"`);
+    return executeShellCommand(`cd ${pwd} && git add --all && git commit -m "Deleted whole directory: ${fname}"`);
+}
+
+
 module.exports = {
     executeShellCommand: executeShellCommand,
-    gitInit: gitInit
+    gitInit: gitInit,
+    gitCommitFileUpload: gitCommitFileUpload,
+    gitCommitIncompleteFileUpload: gitCommitIncompleteFileUpload,
+    gitCommitFileDelete: gitCommitFileDelete,
+    gitCommitDirDelete: gitCommitDirDelete
 };
